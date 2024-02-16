6c3809253cdaeb7b9a18b337ffcdf15c00af86a3
Aan Prayogo <yulian@popbox.asia>
2024-02-16 18:44:26 +0700

    initial commit

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-        res.send('Hello, Express!');
-    }
+    res.send('Hello, Express!');
+  }
 );
 
 app.listen(port, () => {
