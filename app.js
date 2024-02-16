de2b99cb18765c0ac8a85203fc2d2bc1cd27939e
Aan Prayogo <yulian@popbox.asia>
2024-02-16 14:32:26 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const bodyParser = require('body-parser');
+const port = 8081
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+        res.send('Hello, Express!');
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  }
+);
